29d6f1166e4b3034a9079cbe74058a1681f1e85b
carlos79 <Eduardo@Eduardo-W3C>
2016-05-09 19:30:58 -0300

    A classe foi endentada.

--- SigeApp/src/java/beans/LoginBean.java
+++ SigeApp/src/java/beans/LoginBean.java
@@ -10,6 +10,7 @@ import persistencia.UsuarioDAO;
 @ManagedBean
 @SessionScoped
 public class LoginBean {
+
     private Usuario usuario = new Usuario();
 
     public Usuario getUsuario() {
@@ -19,11 +20,11 @@ public class LoginBean {
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
-    
+
     public String autenticar() {
         UsuarioDAO dao = new UsuarioDAO();
         usuario = dao.autentica(usuario.getLogin(), usuario.getSenha());
-        if(usuario == null) {
+        if (usuario == null) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login ou senha incorretos.", ""));
             usuario = new Usuario();
             return "index";
